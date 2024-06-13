fae6494773dbed9a667e9bae789caa778a5bae0b
Lugubriszxc <@Lugubriszxc>
2024-06-13 13:49:50 +0800

    test complete survey design done

--- src/SurveyFinished.tsx
+++ src/SurveyFinished.tsx
@@ -1,21 +1,37 @@
 import imageNote from './assets/images/noteCheckImg.svg'
 import './SurveyFinished.css'
 import Header from './Header.tsx'
+import { Button, Col, Row } from 'reactstrap';
 
 const SurveyFinished = () => {
     return (
         <>
-            <div>
-                <Header />
-                <div className='wholeContainer' id='containerWhole'> {/* Replace '#ADD8E6' with your desired color */}
-                    <div className='imageDiv'>
-                        <img src={imageNote} alt="Note" />
-                    </div>
-                    <div className='hoorayText'>
-                        <p>Hooray!</p>
-                    </div>
-                </div>
+
+            <div className='parentContainer'>
+                <Row className='wholeContainer'>
+                    <Col md="6">
+                        <Header />
+                    </Col>
+                    <Col md="6">
+                        <Col className='imageDiv'>
+                            <img src={imageNote} alt="Note" />
+                        </Col>
+                        <Col className='fontSet hoorayText'>
+                            Hooray!
+                        </Col>
+                        <Col className='fontSet testCompleteText'>
+                            You've completed the test!
+                        </Col>
+                        <Col className='fontSet'>
+                            <Button className='btnResult'>
+                                See Result
+                            </Button>
+                        </Col>
+                    </Col>
+                </Row>
             </div>
+
+
         </>
     );
 }
