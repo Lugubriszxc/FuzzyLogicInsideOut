6d56ad0b4605a802228326f8bab2fc506b82d042
sinadjanMJv2 <sinadjanmichaeljayv2@gmail.com>
2024-06-18 10:32:39 +0800

    arrange the data

--- src/PredictionPage.tsx
+++ src/PredictionPage.tsx
@@ -7,8 +7,26 @@ import Fear from "./assets/fear.svg";
 import Joy from "./assets/joy.svg";
 import { FontAwesomeIcon as Icon } from "@fortawesome/react-fontawesome";
 import "./assets/Card.css";
+import { icon } from "@fortawesome/fontawesome-svg-core";
 
-const emotions = {
+interface Emotions {
+  Sadness: any;
+  Joy: any;
+  Fear: any;
+  Disgust: any;
+  Anger: any;
+  [key: string]: any; // This is the index signature
+}
+
+const emotions: Emotions = {
+  Sadness: Sadness,
+  Joy: Joy,
+  Fear: Fear,
+  Disgust: Disgust,
+  Anger: Anger,
+};
+
+const emotionFullBody: Emotions = {
   Sadness: Sadness,
   Joy: Joy,
   Fear: Fear,
@@ -17,14 +35,24 @@ const emotions = {
 };
 
 const data = [
-  { id: 1, title: "Joy", percentage: "33.3%", icon: Joy }, // Replace Anger with your actual icon import
-  { id: 2, title: "Fear", percentage: "50%", icon: Fear }, // Replace Anger with your actual icon import
-  { id: 3, title: "Anger", percentage: "25.5%", icon: Anger },
-  { id: 5, title: "Sadness", percentage: "25.5%", icon: Disgust },
+  { id: 1, title: "Disgust", percentage: 45.3, },
+  { id: 2, title: "Fear", percentage: 55,},
+  { id: 3, title: "Anger", percentage: 25.5,},
+  { id: 4, title: "Sadness", percentage: 25.5,},
+  { id: 5, title: "Joy", percentage: 60.3,},
 ];
 
 const PredictionPage = () => {
-  const emotionText = "Joy"; // This would be dynamically set based on your logic
+  const highestPercentageItem = data.reduce((prev, current) =>
+    prev.percentage > current.percentage ? prev : current
+  );
+
+  const filteredData = data.filter(
+    (item) => item.percentage !== highestPercentageItem.percentage
+  );
+
+  const emotionText = highestPercentageItem.title;
+
   const emotionImage = emotions[emotionText];
 
   return (
@@ -47,27 +75,64 @@ const PredictionPage = () => {
           />
         )}
 
-        <div className="text-wrapper-6 ">{emotionText}</div>
+        <div className="text-wrapper-6 ">{highestPercentageItem.title}</div>
         <div className="text-wrapper-7 ">
           You're Sadness. Thoughtful and empathetic, you understand the deeper
           emotions that others might overlook. Your ability to connect on an
           emotional level is a true strength.
         </div>
-        <div className="text-wrapper-8">64.7%</div>
+        <div className="text-wrapper-8">
+          {highestPercentageItem.percentage}%
+        </div>
       </div>
 
       <div className="tsx-cards-column">
-        {data.map((item) => (
-          <div className="tsx-card" key={item.id}>
-            <div className="image-container">
-              <img src={item.icon} alt="TSX Icon" className="image" />
+        {filteredData.map((item) => {
+          let color , emotionImages;
+
+          switch (item.title) {
+            case "Joy":
+              color = "#E8DC69";
+              emotionImages = emotions["Joy"];
+              break;
+            case "Fear":
+              color = "#C08CE8";
+              emotionImages = emotions["Fear"];
+              break;
+            case "Sadness":
+              color = "white";
+              emotionImages = emotions["Sadness"];
+              break;
+            case "Anger":
+              color = "#E39DA5";
+              emotionImages = emotions["Anger"];
+              break;
+            case "Disgust":
+              color = "#7BE599";
+              emotionImages = emotions["Disgust"];
+              break;
+            default:
+              color = "white"; // default color if none of the conditions match
+          }
+
+
+
+          return (
+            <div
+              className="tsx-card"
+              key={item.id}
+              style={{ backgroundColor: color }}
+            >
+              <div className="image-container">
+                <img src={emotionImages} alt="TSX Icon" className="image" />
+              </div>
+              <div className="card-content">
+                <div className="title">{item.title}</div>
+                <div className="percentage">{item.percentage}%</div>
+              </div>
             </div>
-            <div className="card-content">
-             <div className="title">{item.title}</div>
-             <div className="percentage">{item.percentage}</div>
-            </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </div>
   );
