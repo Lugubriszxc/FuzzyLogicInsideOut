31595f92cd4aeb9ab573e3a3ef7131c68630036a
ademelmae <113825429+ademelmae@users.noreply.github.com>
2024-06-18 12:58:54 +0800

    connected homepage

--- src/Homepage.tsx
+++ src/Homepage.tsx
@@ -1,24 +1,33 @@
+import React from 'react';
 import { Button, Col, Row } from 'reactstrap';
 import './Homepage.css';
 import Header from './Header';
-
-
+import { useNavigate } from 'react-router-dom';
 
 const HomePage = () => {
-    return (<>
-        <div className="home">
-            <Row className="div">
-                <Col md="6">
-                    <Header />
-                </Col>
-                <Col md="6" className="blue-container">
-                    <p className="text-wrapper-5">Which Inside Out Character Are You?</p>
-                    <p className="text-wrapper-6">Take the prediction test to find out!</p>
-                    <Button className='btn-take'>Take the test</Button>
-                </Col>
-            </Row>
+    const navigate = useNavigate();
+
+    const handleTakeTest = () => {
+        // Navigate with slide-out effect
+        navigate('/survey', { state: { slideDirection: 'left' } });
+    };
+
+    return (
+        <div className="page-container">
+            <div className="home">
+                <Row className="div">
+                    <Col md="6">
+                        <Header />
+                    </Col>
+                    <Col md="6" className="blue-container">
+                        <p className="text-wrapper-5">Which Inside Out Character Are You?</p>
+                        <p className="text-wrapper-6">Take the prediction test to find out!</p>
+                        <Button className='btn-take' onClick={handleTakeTest}>Take the test</Button>
+                    </Col>
+                </Row>
+            </div>
         </div>
-    </>);
+    );
 }
 
 export default HomePage;
